6fd4d39795ff0ac3d47cae7391ae91c24806e13d
mbsig <mbsig@192.168.2.18>
2017-05-15 19:52:43 -0400

    update

--- src/main/java/com/camlait/global/erp/service/validation/ProductModelValidator.java
+++ src/main/java/com/camlait/global/erp/service/validation/ProductModelValidator.java
@@ -2,7 +2,7 @@ package com.camlait.global.erp.service.validation;
 
 import java.util.List;
 
-import org.assertj.core.util.Lists;
+import com.google.common.collect.Lists;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
